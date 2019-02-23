fe656568a623f3d55449071d893082ef368aa906
Roberto Lonardi <lonardi.r@gmail.com>
2019-02-23 12:14:56 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,107 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+import { shuffle } from './utils';
+import { NOTES } from './config';
+
+import styles from './styles.module.css';
+
+const randomNote = () => {
+  const notes = Object.keys(NOTES);
+  return notes[Math.floor((Math.random() * (notes.length - 1)))];
+};
+
+const getCorrectnessClass = correct => {
+  const classes = [styles.correctness];
+
+  if (correct) classes.push(styles.correct);
+  else classes.push(styles.wrong);
+
+  return classes.join(' ');
+}
+
+export default class App extends Component {
+
+  state = {
+    wrongs       : 0,
+    rights       : 0,
+    disabled     : false,
+    note         : randomNote(),
+    selectedNote : undefined,
+    correct      : undefined
+  }
+
+  selectAnswer = usNote => {
+    const correct = usNote === NOTES[this.state.note];
+
+    const { rights, wrongs } = this.state;
+
+    this.setState({
+      correct,
+      disabled     : true,
+      selectedNote : usNote,
+      rights       : correct ? (rights + 1) : rights,
+      wrongs       : !correct ? (wrongs + 1) : wrongs
+    })
+
+    setTimeout(() => {
+      this.setState({
+        disabled     : false,
+        note         : randomNote(),
+        selectedNote : undefined,
+        correct      : undefined
+      })
+    }, 1000)
+
+  }
+
   render() {
+
+    const {
+
+      note,
+      correct,
+      disabled,
+
+      rights,
+      wrongs
+
+    } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div clasName={styles.container}>
+
+        <div className={styles.counter}>
+          <div className={styles.right}>
+            Right: { rights }
+          </div>
+          <div className={styles.wrong}>
+            Wrong: { wrongs }
+          </div>
+        </div>
+
+        <div className={styles.note}>
+          <span className={styles.inner}>
+            { note }
+          </span>
+        </div>
+
+        <ul className={styles.selection}>
+          { shuffle(Object.values(NOTES)).map((usNote, idx) => (
+            <li
+              className={styles.item}
+              onClick={() => {
+                if (!disabled) this.selectAnswer(usNote)
+              }}>
+              { usNote }
+            </li>
+          )) }
+        </ul>
+
+        { correct !== undefined && <div className={getCorrectnessClass(correct)}>
+          { correct ? 'Right' : 'Wrong' }
+        </div> }
+
       </div>
     );
   }
 }
-
-export default App;
---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+export const NOTES = {
+  Do: 'C',
+  Re: 'D',
+  Mi: 'E',
+  Fa: 'F',
+  Sol: 'G',
+  La: 'A',
+  Si: 'B'
+}
